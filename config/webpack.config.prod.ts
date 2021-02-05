b9326354a91974ef30da028b68b5afb2d8ad5e77
asurance <784672616@qq.com>
2021-02-05 21:59:07 +0800

    🔧build: 更新配置,发布 0.0.1

--- config/webpack.config.prod.ts
+++ config/webpack.config.prod.ts
@@ -1,13 +1,18 @@
 import Merge from 'webpack-merge'
 import BaseConfig from './webpack.config.base'
-import { Configuration } from 'webpack'
+import { Configuration, WebpackPluginInstance } from 'webpack'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
 
 const config = {
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin()
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({ extractComments: false }) as unknown as WebpackPluginInstance]
+    }
 } as Configuration
 
 export default Merge(BaseConfig, config)
